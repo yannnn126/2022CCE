0a88a176ffd3e95930c81f206d04f3ebd4f2db8f
unknown <alex930688@gmail.com>
2023-02-14 11:59:25 +0800

    week01

--- week01/week01-2.cpp
+++ week01/week01-2.cpp
@@ -1,11 +1,15 @@
 #include<stdio.h>
-
 int main()
 {
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("我是黃承彥\n");
-    ///有時候中文會有問題,你可以用英文
-    //註解 comments是給人看的 作業2加註解
-    ///CodeBlocks如果畫三條,會比較深
+    printf("請輸入一個字母: ");
+    char c;
+    scanf("%c",&c);
+    printf("你輸了%c\n",c);
+
+    printf("請輸入一個整數: ");
+    int a;
+    scanf("%d",&a);
+    printf("你輸了%d\n",a);
+    return 0;
+
 }
